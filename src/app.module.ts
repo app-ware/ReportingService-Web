148d0c5e29affadd1eb62593506698d54a8f9237
Osama Saad <osamasaadabuelyazeed@gmail.com>
2025-10-13 04:47:02 +0300

    refactor(app-module , main ) : refactor app.module and main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,44 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformInterceptor } from './common/json-respons-files/transform.interceptor';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { icareDbConfig, isecureDbConfig } from './config/database.config';
+import { HttpExceptionFilter } from './common/json-respons-files/http-exception.filter';
+import { MssqlClientModule } from './config/mssql/mssql-client.module';
+import { AppLoggerModule } from './common/logger/logger.module';
+import { EvaluationReportsModule } from './modules/evaluation-report/evaluation-reports.module';
+
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    //Common imports
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(icareDbConfig),
+    TypeOrmModule.forRoot(isecureDbConfig),
+    MssqlClientModule,AppLoggerModule,
+
+    //Feature Modules  
+    EvaluationReportsModule,
+  ],
+  providers: [
+    { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+
+  ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppValidationPipe } from './common/pipes/validation.pipe';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule,{bufferLogs:true,logger:false});
+
+  app.useLogger(app.get(Logger))
+  app.useGlobalPipes(AppValidationPipe)
+  app.setGlobalPrefix('api/v1');
+  const port = process.env.PORT || 4445;
+  await app.listen(port);
+
+  const logger = app.get(Logger);
+  logger.log(`Reporting service is running on port ${port}`)
 }
-bootstrap();
+void bootstrap();
